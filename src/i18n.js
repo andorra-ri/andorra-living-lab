7b8169938e7f555f88aa619b4363b78aaf0f0e94
mvilella <mvilella@obsa.ad>
2021-11-22 13:16:00 +0100

    feat: Add localization with i18n

---
+++ src/i18n.js
@@ -0,0 +1,9 @@
+import { createI18n } from 'vue-i18n';
+/* eslint-disable-next-line import/no-unresolved */
+import messages from '@intlify/vite-plugin-vue-i18n/messages';
+
+export default createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages,
+});
--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,19 @@
 import { useAirtable } from 'painless-airtable';
+import i18n from '/@/i18n';
 import config from '/@/config.yaml';
+
+const { locale } = i18n.global;
 
 const { base, resources } = config.api;
 const token = import.meta.env.VITE_AIRTABLE_TOKEN;
 
 const airtable = useAirtable({ base, token });
+
+// Define localizable fields for the API query
+const localize = options => {
+  const { localized = [], ...rest } = options;
+  const fields = rest.fields.map(field => (
+    localized.includes(field) ? `${field}_${locale.value}` : field
+  ));
+  return { ...rest, fields };
+};
