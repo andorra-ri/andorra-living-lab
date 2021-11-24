918492dcbd2b22571aa4db1a52ec7e5d90b3fbb7
mvilella <mvilella@obsa.ad>
2021-11-24 10:49:30 +0100

    feat: Add Living Lab introduction

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
     <landing />
+    <living-lab />
   </div>
 </template>
 
 <script>
 import Landing from '/@/views/Landing.vue';
+import LivingLab from '/@/views/LivingLab.vue';
 
 export default {
   name: 'App',
   components: {
     Landing,
+    LivingLab,
   },
 };
 </script>
---
+++ src/views/LivingLab.vue
@@ -0,0 +1,54 @@
+<template>
+  <section id="living-lab" class="section light">
+    <div class="container columns columns--3 shifted" style="--shift:100px">
+      <div class="column box">
+        <figure class="cover">
+          <img :src="images.thumbnail.path">
+          <figcaption v-if="images.thumbnail.caption">{{ images.thumbnail.caption }}</figcaption>
+        </figure>
+      </div>
+      <div class="column box" />
+      <div class="column box secondary">
+        <h4>{{ t('livinglab.join') }}</h4>
+        <p>{{ t('livinglab.join_call') }}</p>
+        <p><a :href="join.url" class="button">{{ t('livinglab.join_now') }}</a></p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="columns columns--2">
+        <div class="column">
+          <i18n-t tag="h2" keypath="livinglab.title">
+            <template #strong><strong>{{ t('livinglab.title_em') }}</strong></template>
+          </i18n-t>
+          <p>{{ t('livinglab.description') }}</p>
+          <ul class="bullets cols-2">
+            <li>Agility in the development of pilots.</li>
+            <li>Innovation friendly regulation.</li>
+            <li>Proximity between key partners.</li>
+            <li>Transversal support from public &amp; private sectors.</li>
+          </ul>
+        </div>
+        <div class="column" />
+      </div>
+      <div class="columns columns--4">
+        <article v-for="benefit in benefits" :key="benefit" class="column">
+          <h3 class="lined">{{ t(`livinglab.benefits.${benefit}.title`) }}</h3>
+          <p>{{ t(`livinglab.benefits.${benefit}.description`) }}</p>
+        </article>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n';
+import { livinglab } from '/@/config.yaml';
+
+export default {
+  name: 'LivingLab',
+  setup() {
+    const { t } = useI18n();
+    return { t, ...livinglab };
+  },
+};
+</script>
