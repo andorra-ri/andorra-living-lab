cf42284d73c33ed7d124b1bdb3979515c7c35e0a
mvilella <mvilella@obsa.ad>
2022-01-12 19:46:44 +0100

    feat: Load project and parse markdown

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,8 +1,18 @@
 <template>
   <div v-if="project">
     <header class="section primary">
+      <img :src="project.cover[0].url" class="cover shaded" draggable="false">
     </header>
     <section class="section">
+      <div class="container">
+        <aside class="light">
+        </aside>
+        <div class="main">
+          <h1>{{ project.name }}</h1>
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <div class="md" v-html="project.description" />
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -11,6 +21,8 @@
 import { ref, onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { useRoute } from 'vue-router';
+import { Remarkable } from 'remarkable';
+import { getProjects } from '/@/services/api.service';
 
 export default {
   name: 'Project',
@@ -19,6 +31,21 @@ export default {
     const { params } = useRoute();
     const project = ref(undefined);
 
+    const md = new Remarkable('full', {
+      html: true,
+      breaks: true,
+      typographer: true,
+    });
+
+    onMounted(async () => {
+      const [{
+        [`name_${locale.value}`]: name,
+        [`description_${locale.value}`]: description,
+        ...rest
+      }] = await getProjects(params.slug);
+      project.value = { ...rest, name, description: md.render(description) };
+    });
+
     return { t, locale, project };
   },
 };
@@ -26,4 +53,15 @@ export default {
 
 <style lang="scss" scoped>
 header.section { height: 10rem; }
+
+.container {
+  display: flex;
+  gap: 2rem;
+
+  aside {
+    flex: 0 0 18rem;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+}
 </style>
