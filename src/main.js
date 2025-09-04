1791e9e58a309e505e98b015d56a4d155e797a82
Marc Jorge <mjorge@ari.ad>
2025-09-04 12:34:17 +0200

    chore(core): implement pinia stores

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import InlineSvg from 'vue-svg-inline-plugin';
+import { createPinia } from 'pinia';
 import router from './router';
 import i18n from './i18n';
 import App from './App.vue';
@@ -7,7 +8,10 @@ import App from './App.vue';
 import 'reset-css';
 import './styles/main.scss';
 
+const pinia = createPinia()
 const app = createApp(App);
+
+app.use(pinia)
 app.use(InlineSvg);
 app.use(router);
 app.use(i18n);
