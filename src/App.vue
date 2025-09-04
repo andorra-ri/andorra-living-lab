885caf78bdae16715258437ecd9cfb1753786def
Marc Jorge <mjorge@ari.ad>
2025-09-04 12:38:40 +0200

    chore(core): App - prefetch all from stores onBeforeMount

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,31 @@
 </template>
 
 <script>
+import { onBeforeMount } from 'vue';
+import { useChallengesStore } from '/@/stores/challengesStore';
+import { usePartnersStore } from '/@/stores/partnersStore';
+import { useTestimonialsStore } from '/@/stores/testimonialsStore'
+import { useProjectsStore } from '/@/stores/projectsStore'
 import Navigation from '/@/views/Navigation.vue';
 import PageFooter from '/@/views/PageFooter.vue';
 
 export default {
   name: 'App',
   components: { Navigation, PageFooter },
+  setup() {
+    const challengeStore = useChallengesStore();
+    const partnersStore = usePartnersStore();
+    const testimonialsStore = useTestimonialsStore();
+    const projectsStore = useProjectsStore();
+
+    onBeforeMount(async () => {
+      Promise.all([
+        challengeStore.getAllChallenges(),
+        partnersStore.getAllPartners(),
+        testimonialsStore.getAllTestimonials(),
+        projectsStore.getAllProjects()
+      ])
+    });
+  }
 };
 </script>
